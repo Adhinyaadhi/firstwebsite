88d53916586fa5991c098e797b41ff2e0137f560
Adhinyaadhi <adhidivi2002@gmail.com>
2024-04-18 18:22:38 +0530

    dowmloads are removed

--- javascript/Downloads/calculator-script.js
+++
@@ -1,7 +0,0 @@
-function displayValue(val) {  
-    document.getElementById("display").value = document.getElementById("display").value + val;
-}
-
-function clearDisplay() {
-    document.getElementById("display").value = ""
-}
